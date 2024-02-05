f08eede26d89aa5e9f813ee027251030c87df511
hegade <subramanyashegade@gmail.com>
2024-02-05 15:57:09 +0530

    chore: added contnet loader

---
+++ src/components/UI/ContentLoader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ThreeDots } from "react-loader-spinner";
+
+const ContentLoader = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "center",width:'100%',alignItems:'center' }}>
+      <ThreeDots color="blue" width={"50px"} height={"50px"} />
+    </div>
+  );
+};
+
+export { ContentLoader };
--- src/components/Withdraw/WithdrawTable.jsx
+++ src/components/Withdraw/WithdrawTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ButtonWithLoading from "../UI/LoaderButton";
 import { useStacks } from "../../providers/StacksProvider";
 import { useTransactionToasts } from "../../providers/TransactionStatusProvider";
@@ -9,24 +9,19 @@ import {
   deployedContractName,
 } from "../../lib/constants";
 
-import {
-  createAssetInfo,
-  FungibleConditionCode,
-  makeStandardFungiblePostCondition,
-  uintCV,
-  tupleCV,
-  principalCV,
-  stringAsciiCV,
-} from "@stacks/transactions";
+import { uintCV, principalCV } from "@stacks/transactions";
 import { getContractAddressAndName } from "../../utils/extract-contract-info";
 import { openContractCall } from "@stacks/connect";
 import { withDrawData } from "../../lib/withdraw-data";
 import { shortAddress } from "../../utils/format/address.format";
+import { ContentLoader } from "../UI/ContentLoader";
 
 const WithdrawTable = () => {
   const { network, address } = useStacks();
-  
+
   const { addTransactionToast } = useTransactionToasts();
+
+  const [loading, setLoading] = useState(true);
 
   const handleWithdraw = async ({ amount, assetName, tokenAddress }) => {
     const { contractAddress, contractName } =
@@ -60,81 +55,85 @@ const WithdrawTable = () => {
     }
   };
 
+  if (loading) {
+   return <ContentLoader />;
+  }
   return (
     <>
       <div className="table-body same-color-rows">
-        {
-          withDrawData.map(token=>{
-
-            return <>
-            <div className="table-row medium">
-          <div className="table-column">
-            <div className="product-preview tiny">
-              <a>
-                <figure
-                  className="product-preview-image short liquid"
-                  style={{ background: "url('img/btc-logo.svg')" }}
-                >
-                  <img
-                    src="img/btc-logo.svg"
-                    alt="item-11"
-                    style={{ display: "none" }}
-                  />
-                </figure>
-              </a>
-              <div className="product-preview-info">
-                <p className="product-preview-title">{token.ftName}</p>
+        {withDrawData.map((token) => {
+          return (
+            <>
+              <div className="table-row medium" key={token.LockID}>
+                <div className="table-column">
+                  <div className="product-preview tiny">
+                    <a>
+                      <figure
+                        className="product-preview-image short liquid"
+                        style={{ background: "url('img/btc-logo.svg')" }}
+                      >
+                        <img
+                          src="img/btc-logo.svg"
+                          alt="item-11"
+                          style={{ display: "none" }}
+                        />
+                      </figure>
+                    </a>
+                    <div className="product-preview-info">
+                      <p className="product-preview-title">{token.ftName}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="table-column padded">
+                  <p className="table-title">
+                    {shortAddress(token.ftContract.split(".")[0])}
+                  </p>
+                </div>
+                <div className="table-column padded">
+                  <p className="table-title">{token.amount} $BRCL</p>
+                </div>
+                <div className="table-column padded">
+                  <p className="table-title">Wed, 09 Mar 2022 12:09:47 GMT</p>
+                </div>
+                <div className="table-column padded">
+                  <div id="clockdiv">
+                    <div>
+                      <span className="days">99</span>
+                      <div className="smalltext">D</div>
+                    </div>
+                    <div>
+                      <span className="hours">23</span>
+                      <div className="smalltext">H</div>
+                    </div>
+                    <div>
+                      <span className="minutes">54</span>
+                      <div className="smalltext">M</div>
+                    </div>
+                    <div>
+                      <span className="seconds">57</span>
+                      <div className="smalltext">S</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="table-column padded-left">
+                  <div className="table-actions">
+                    <ButtonWithLoading
+                      onClick={() =>
+                        handleWithdraw({
+                          amount: token.amount,
+                          tokenAddress: token.ftContract,
+                          assetName: token.ftName,
+                        })
+                      }
+                      text="withdraw"
+                      className="button secondary"
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="table-column padded">
-            <p className="table-title">{shortAddress(token.ftContract.split('.')[0])}</p>
-          </div>
-          <div className="table-column padded">
-            <p className="table-title">{token.amount} $BRCL</p>
-          </div>
-          <div className="table-column padded">
-            <p className="table-title">Wed, 09 Mar 2022 12:09:47 GMT</p>
-          </div>
-          <div className="table-column padded">
-            <div id="clockdiv">
-              <div>
-                <span className="days">99</span>
-                <div className="smalltext">D</div>
-              </div>
-              <div>
-                <span className="hours">23</span>
-                <div className="smalltext">H</div>
-              </div>
-              <div>
-                <span className="minutes">54</span>
-                <div className="smalltext">M</div>
-              </div>
-              <div>
-                <span className="seconds">57</span>
-                <div className="smalltext">S</div>
-              </div>
-            </div>
-          </div>
-          <div className="table-column padded-left">
-            <div className="table-actions">
-              <ButtonWithLoading
-                onClick={() =>
-                  handleWithdraw({
-                    amount: token.amount,
-                    tokenAddress:token.ftContract,
-                    assetName: token.ftName,
-                  })
-                }
-                text="withdraw"
-                className="button secondary"
-              />
-            </div>
-          </div>
-        </div>
             </>
-          })
-        }
+          );
+        })}
       </div>
     </>
   );
