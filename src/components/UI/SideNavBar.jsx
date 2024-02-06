73d60c74870dd9ab8f2b512b77a3a9f05bd05f59
hegade <subramanyashegade@gmail.com>
2024-02-06 13:08:09 +0530

    feat: added navigatin heighlight

--- src/components/UI/SideNavBar.jsx
+++ src/components/UI/SideNavBar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const SideNavBar = () => {
+  const location = useLocation();
+
   return (
     <>
       <nav
@@ -9,8 +11,8 @@ const SideNavBar = () => {
         data-simplebar
       >
         <ul className="menu">
-          <li className="menu-item active">
-            <a className="menu-item-link" href="#">
+          <li className={"menu-item" + (location.pathname === "/dash-board" ? " active" : "") }>
+            <Link className="menu-item-link" to="/dash-board">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
@@ -22,7 +24,7 @@ const SideNavBar = () => {
                 <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4z" />
               </svg>
               Dashboard
-            </a>
+            </Link>
           </li>
           <li className="menu-item">
             <a className="menu-item-link" href="#">
@@ -39,7 +41,7 @@ const SideNavBar = () => {
               My Token Lock Schedule
             </a>
           </li>
-          <li className="menu-item">
+          <li className={"menu-item" + (location.pathname === "/token-withdraw" ? " active" : "")}>
             <Link className="menu-item-link" to="/token-withdraw">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -72,7 +74,7 @@ const SideNavBar = () => {
               Pools
             </a>
           </li>
-          <li className="menu-item">
+          <li className={"menu-item" + (location.pathname === "/token-lock" ? " active" : "")}>
             <Link className="menu-item-link" to="/token-lock">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
